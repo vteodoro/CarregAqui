b06a89cfa062aab4f3d825251554cd67a7abda24
vteodoro <viviteodoro19@gmail.com>
2018-04-21 00:07:46 -0300

    Editar classes - Recarregar Activity

--- CarregAquiApp/app/src/main/java/br/com/example/mobile/carregaquiapp/view/RecarActivity.java
+++ CarregAquiApp/app/src/main/java/br/com/example/mobile/carregaquiapp/view/RecarActivity.java
@@ -12,33 +12,40 @@ import br.com.example.mobile.carregaquiapp.model.Saldo;
 import br.com.example.mobile.carregaquiapp.view.HomeActivity;
 
 public class RecarActivity extends Activity{
+    Saldo sd = new Saldo();
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recarregar);
+
     }
 
     public void recarCard (View view){
+//
+//        EditText saldo1 = findViewById(R.id.saldo1Rec);
+//        EditText saldo2 = findViewById(R.id.saldo2Rec);
+//
+//
+//        if((saldo1.getText().toString().isEmpty())&&(saldo2.getText().toString().isEmpty())){
+//            Toast.makeText(this, "Insira um valor", Toast.LENGTH_SHORT).show();
+//        }
+//        else{
+//            Toast.makeText(this, "Valor creditado", Toast.LENGTH_SHORT).show();
+//
+//            Bundle bundle = new Bundle();
+//            bundle.putString("s1", saldo1.getText().toString());
+//            bundle.putString("s2", saldo2.getText().toString());
+//
 
-        EditText saldo1 = findViewById(R.id.saldo1Rec);
-        EditText saldo2 = findViewById(R.id.saldo2Rec);
-
-
-        if((saldo1.getText().toString().isEmpty())&&(saldo2.getText().toString().isEmpty())){
-            Toast.makeText(this, "Insira um valor", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(this, "Valor creditado", Toast.LENGTH_SHORT).show();
-
-            Bundle bundle = new Bundle();
-            bundle.putString("s1", saldo1.getText().toString());
-            bundle.putString("s2", saldo2.getText().toString());
-
+            sd.setSaldo01(R.id.saldo1Rec);
+            sd.setSaldo02(R.id.saldo2Rec);
+            
             Intent intent = new Intent(this, HomeActivity.class);
-            intent.putExtras(bundle);
+            //intent.putExtras(bundle);
             startActivity(intent);
 
             finish();
         }
     }
-}
+//}
